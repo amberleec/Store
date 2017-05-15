2d1c63ce8d4c476af731a7a50473b771cce5f73c
Amberlee <amberleec12@yahoo.com>
2017-05-15 14:13:22 -0500

    Have all the sout's displayed now time to key in the functions

--- src/storeExercise.java
+++ src/storeExercise.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 
 /**
- * Created by Amberlee on 5/15/17.
+ * Created by
+ * Amberlee on 5/15/17.
  */
+
 public class storeExercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
     }
 
     public static void displayMenu(){
-        System.out.println();
+
+        System.out.println("What is your name?");
+        System.out.println("What item would you like?");
+        System.out.println("Price fot item " );
+        System.out.println("$");
+        System.out.println("Would you like to continue? y/n");
+
+        System.out.println("here is your receipt");
+        System.out.println("Thank you for shopping with our store!");
+
     }
 }
