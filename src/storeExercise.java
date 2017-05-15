5be6963457b32676bbded266b02739167060e765
Amberlee <amberleec12@yahoo.com>
2017-05-15 13:57:42 -0500

    Start of store

---
+++ src/storeExercise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Amberlee on 5/15/17.
+ */
+public class storeExercise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+
+    public static void displayMenu(){
+        System.out.println();
+    }
+}
