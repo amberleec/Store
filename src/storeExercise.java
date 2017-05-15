2755e990486b1de9c0d2ae3178fe98c8ddea8549
Amberlee <amberleec12@yahoo.com>
2017-05-15 16:00:01 -0500

    Added the while loop and final message

--- src/storeExercise.java
+++ src/storeExercise.java
@@ -12,42 +12,44 @@ public class storeExercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         PrintStream out = new PrintStream(System.out);
-        String menu = "y",receipt = "",item = "";
+        String menu = "y", receipt = "", item = "";
         double total = 0, price = 0;
-        int quantity,itemNo = 1;
+        int quantity, itemNo = 1;
 
 
+        while (menu.equalsIgnoreCase("y")) {
 
+            displayMenu(out);
+            switch (options(input, out)) {
+                case 1:
+                    item = userItem(input, out);
+                    price = userPrice(input, out);
+                    quantity = userQuantity(input, out);
+                    receipt += "\n" + itemNo + "." + quantity + "x" + item + " " + amount(price);
+                    total += quantity * price;
+                    itemNo++;
+                    break;
+                case 2:
+                    System.out.println("*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*");
+                    System.out.println("Thank you for shopping with our store. We will see you next time!");
+                    break;
+                case 3:
+                    System.out.println("Thank you! Have a good day!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid! Please select between 1-3!");
 
-        displayMenu(out);
-        switch(options(input,out)){
-            case 1:
-                item = userItem(input, out);
-                price = userPrice(input, out);
-                quantity = userQuantity(input, out);
-                receipt += "\n" + itemNo + "." + quantity + "x" + item + " " + amount(price);
-                total += quantity * price;
-                itemNo++;
-                break;
-            case 2:
-                System.out.println("*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*");
-                System.out.println("Thank you for shopping with our store. We will see you next time!");
-                break;
-            case 3:
-                System.out.println("Thank you! Have a good day!");
-                System.exit(0);
-            default:
-                System.out.println("Invalid! Please select between 1-3!");
+            }
+
+            System.out.println("Would you like to return to the menu? y/n");
+            menu = input.nextLine();
 
         }
 
-        System.out.println("Would you like to return to the menu? y/n");
-        menu = input.nextLine();
-
-
+        System.out.println("Thank you for shopping with us!");
     }
 
-    public static void displayMenu(PrintStream out){
+    public static void displayMenu(PrintStream out) {
 //        System.out.println("What is your name?");
 //        String name = input.nextLine();
         System.out.println("****Welcome to our store****");
@@ -56,39 +58,40 @@ public class storeExercise {
         System.out.println("2) Receipt");
         System.out.println("3) Exit");
 
+
     }
 
-    public static String name(Scanner input,PrintStream out) {
+    public static String name(Scanner input, PrintStream out) {
         System.out.println("What is your name?");
         return input.nextLine();
     }
 
-    public static int options(Scanner input,PrintStream out){
+    public static int options(Scanner input, PrintStream out) {
         int option = input.nextInt();
         input.nextLine();
         return option;
     }
 
-    public static String userItem(Scanner input,PrintStream out){
+    public static String userItem(Scanner input, PrintStream out) {
         System.out.println("What item would you like to buy?");
         return input.nextLine();
     }
 
-    public static double userPrice(Scanner input,PrintStream out){
+    public static double userPrice(Scanner input, PrintStream out) {
         System.out.println("How much is the price for that item?");
         return input.nextDouble();
 
     }
 
-    public static int userQuantity(Scanner input,PrintStream out){
+    public static int userQuantity(Scanner input, PrintStream out) {
         System.out.println("How many would you like to buy?");
         int quantity = input.nextInt();
         input.nextLine();
         return quantity;
     }
 
-    public static String amount(double number){
-        return "$" + String.format("%,.2f",number);
+    public static String amount(double number) {
+        return "$" + String.format("%,.2f", number);
     }
 
 }
